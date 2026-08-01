68fc523670778595e689a5e2fa7c92a7b0afb68d
Javier Estrada <javiera.estradag@gmail.com>
2026-08-01 11:13:54 -0600

    Corrige botones cancelar de modales de carreras y ciclos

--- src/pages/CareersPage.tsx
+++ src/pages/CareersPage.tsx
@@ -359,7 +359,7 @@ export const CareersPage: React.FC = () => {
             <div className="flex justify-end gap-3 pt-4 border-t border-[#E2E8F0]">
               <button
                 type="button"
-                onClick={() => setShowAddModal(false)}
+                onClick={() => { setShowAddModal(false); setShowEditModal(false); }}
                 className="rounded-lg border border-[#E2E8F0] px-4 py-2 text-xs font-medium text-[#333333]"
               >
                 Cancelar
